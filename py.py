0165e0938ee588e82a6bdbc8bdf6207ae818a95f
ProgramerB <Bhavesh2000@outlook.com>
2023-01-06 20:31:41 +0530

    added clear button

--- py.py
+++ py.py
@@ -10,7 +10,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template("my-form.html") # this should be the name of your html file
+    temp = 'empty'
+    try:
+        conn = sqlite3.connect('spider.sqlite')
+        cur = conn.cursor()
+        cur.execute('''Select url from Webs''')
+        for row in cur:
+            temp = row[0]
+        cur.close()
+    except:
+        pass
+    return render_template("my-form.html",site=temp) # this should be the name of your html file
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -31,15 +41,20 @@ def my_form_post():
         spjson.spjson(int(text2))
         return render_template("table.html")
     elif 'spremove' in request.form:
-        conn = sqlite3.connect('spider.sqlite')
-        cur = conn.cursor()
-        cur.execute('''DROP TABLE Pages''')
-        cur.execute('''DROP TABLE Links''')
-        cur.execute('''DROP TABLE Webs''')
-        conn.commit()
-        cur.close()
-
+        try:
+            conn = sqlite3.connect('spider.sqlite')
+            cur = conn.cursor()
+            cur.execute('''DROP TABLE Pages''')
+            cur.execute('''DROP TABLE Links''')
+            cur.execute('''DROP TABLE Webs''')
+            conn.commit()
+            cur.close()
+        except:
+            pass
+        fhand = open('static/javascript/spider.js', 'w')
+        fhand.close()
         print("All tables droped")
+        return render_template("table.html")
 
 @app.route('/chart')
 def chart():
@@ -56,9 +71,6 @@ def table():
 @app.route('/weight')
 def weight():
     return render_template("weight.html")
-
-    
-
 
 if __name__ == '__main__':
     app.run()
